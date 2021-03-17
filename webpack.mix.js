d8710ed11d0e8677c38895e675f858bf50aec8ff
maganasamuel <maganasamuel001@gmail.com>
2021-03-18 06:53:38 +0800

    Used tailwind cdn instead of npm

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,4 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-    .postCss('resources/assets/css/app.css', 'public/css');
+mix.js('resources/assets/js/app.js', 'public/js');
