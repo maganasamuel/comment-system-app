459e8304eaae473114d6482fb346919f1b4fe246
maganasamuel <maganasamuel001@gmail.com>
2021-03-18 12:16:04 +0800

    fetch comments, and created comment

--- app/Http/Controllers/Api/CommentController.php
+++ app/Http/Controllers/Api/CommentController.php
@@ -3,22 +3,42 @@
 namespace App\Http\Controllers\Api;
 
 use App\Actions\PostComment;
+use App\Comment;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\CommentResource;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    public function index()
+    public function index($comment = null)
     {
-        return response()->json([
-            'message' => 'hello world',
-        ]);
+        $query = new Comment();
+
+        if ($comment) {
+            $parentComment = Comment::findOrFail($comment);
+
+            $query = $parentComment->children();
+        } else {
+            $query = $query->whereNull('parent_id');
+        }
+
+        $offset = request()->query('offset', 0);
+
+        $count = $query->count();
+
+        $comments = $query
+            ->orderBy('created_at', 'desc')
+            ->orderBy('id', 'desc')
+            ->offset($offset)->limit(15)
+            ->get();
+
+        return CommentResource::collection($comments)->additional(['count' => $count]);
     }
 
     public function store($comment = null, PostComment $action)
     {
         $createdComment = $action->post($comment);
 
-        return response()->json($createdComment);
+        return new CommentResource($createdComment);
     }
 }
